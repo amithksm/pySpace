1ecf65473d802a864c4d2ac8499db453b5e3637f
amith_k <amithkumar.sm@gmail.com>
2020-08-02 22:14:22 +0530

    first commit

---
+++ wordcount.py
@@ -0,0 +1,11 @@
+from urllib.request import urlopen
+
+contents = "Betty Botter bought a bit of butter \n but the bit of butter was bitter so Betty Botter bought a bit of better butter \nto make the bit of bitter butter better"
+total_words = contents.split()
+unique = set(contents.split())
+words_count = {}
+for w in unique:
+    words_count.update({w:total_words.count(w)})    
+
+print(str(words_count))
+
